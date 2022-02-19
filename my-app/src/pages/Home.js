e01e45f6ba9f37065d32efda2ff1d181a7b916bc
ParnKrub <parn2544krub@gmail.com>
2022-02-19 09:43:29 +0700

    added initial pages

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-      <div className='container'>
-          <h2>Home</h2>
-          <Link to="/Info"><h3>Get more Info</h3></Link>
-          <Link to="/Graph"><h3>Get Temperature Graph</h3></Link>
-      </div>
-      
-      
+    <div className='container'>
+      <h2>Home</h2>
+      <Link to="/Info"><h3>Get more Info</h3></Link>
+      <Link to="/Graph"><h3>Get Temperature Graph</h3></Link>
+    </div>
+
+
   )
 }
 
