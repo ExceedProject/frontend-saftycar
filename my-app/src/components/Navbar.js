7ff73434796cdad3237b5f56a760600ab9181f17
ParnKrub <parn2544krub@gmail.com>
2022-02-19 17:13:01 +0700

    build some graph, change some pages

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,12 +1,13 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { AiFillCar,AiFillHome } from "react-icons/ai"
 
 const Navbar = () => {
   return (
       <nav>
-          <h1>Title</h1>
-          <Link to="/Home"><h3>Home</h3></Link>
+          <h1><AiFillCar /> SAFETYCAR - EXCEED16<AiFillCar /></h1>
+          <Link to="/Home"><h3><AiFillHome /> Home</h3></Link>
       </nav>
   )
 }
--- my-app/src/pages/Graph.js
+++ my-app/src/pages/Graph.js
@@ -1,9 +1,58 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts'
+import moment from 'moment'
 
 const Graph = () => {
+
+  const [graphData, setGraphData] = useState([]);
+  useEffect(() => {
+    getData().then((data) => {
+      setGraphData(data)
+    },3000)
+  }, [])
+  
+  // console.log(graphData)
+
+  const dateFormatter = date => {
+    return moment(date).format('DD/MM/YY HH:mm');
+  }
+
+
+  async function getData() {
+    const res = await axios.get('https://ecourse.cpe.ku.ac.th/exceed16/api/safety-car/graph')
+    return res.data
+  }
   return (
-    <div>Graph</div>
+    <div className='graph'>
+      <div className='carbon-graph'>
+        <div className='carbon-label'>Carbon Monoxide Graph</div>
+        <p>Carbon Monoxide Level (ppm)</p>
+        <LineChart width={800} height={400} data={graphData}> 
+        <XAxis
+            dataKey="time"
+            tickFormatter={dateFormatter}
+        />
+        <YAxis />
+        <CartesianGrid stroke="#ccc" />
+        <Line type="monotone" dataKey="carbon" stroke="#8884d8" strokeWidth={3} />
+        </LineChart>
+      </div>
+      <div className='heat-graph'>
+        <div className='heat-label'>Heat Graph</div>
+        <p>Car Temperature (C)</p>
+        <LineChart width={800} height={400} data={graphData}>
+        <XAxis
+            dataKey="time"
+            tickFormatter={dateFormatter}
+        />
+        <YAxis/>
+        <CartesianGrid stroke="#ccc" />
+        <Line type="monotone" dataKey="heat" stroke="orangered" strokeWidth={3} />
+        </LineChart>
+      </div>
+    </div>
   )
 }
 
