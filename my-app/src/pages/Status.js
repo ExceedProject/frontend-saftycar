325c84bc8ecf3c8ff388b973a07bb7d81f680125
Kittison <kittison.j@ku.th>
2022-02-19 22:39:39 +0700

    add warning

--- my-app/src/pages/Status.js
+++ my-app/src/pages/Status.js
@@ -16,6 +16,9 @@ const Status = ()=>  {
             getSafecheck().then((data) =>{
                 console.log(data)
                 setSafechaeck(data)
+                // if(data["warning"]===1){
+                //     window.alert('May have a human in that place?')
+                // }
             })
         
         }, 3000);
@@ -23,6 +26,11 @@ const Status = ()=>  {
             clearInterval(id)
         }
     }, [])
+    useEffect(() => {
+        if(Safecheck["warning"]===1){
+            window.alert('May have a human in that place?')
+        }
+    }, [Safecheck?.warning])
 
 
 async function getStatusdata(){
@@ -43,22 +51,22 @@ console.log("this = ", Safecheck)
     <div  style={{display:'flex',alignItems:'center',justifyContent:'center',padding:150}}>
         <img src={ "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fsites%2Fjimgorzelany%2F2019%2F07%2F23%2Fhere-are-the-coolest-new-cars-for-2020%2F&psig=AOvVaw2Jg2gOfHVq5D2QfRWmsQA-&ust=1645354916764000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCB9-3Oi_YCFQAAAAAdAAAAABAJ" }/>
         <div className="upper" style={{marginLeft:20}}>   
-        <h1>Status</h1>
-        <div className="showstatus">
+            <h1>Status</h1>
+            <div className="showstatus">
+                <div style={{display:'flex',alignItems:'center',justifyContent:'space-between'}}>
+                  <h1>CO :</h1><div className="circle" style={{backgroundColor: Statusdata["carbon_color"],width:20,height:20,borderRadius:'50%'}}/>  
+                </div>
             <div style={{display:'flex',alignItems:'center',justifyContent:'space-between'}}>
-            <h1>CO :</h1><div className="circle" style={{backgroundColor: Statusdata["carbon_color"],width:20,height:20,borderRadius:'50%'}}/>  
+            <h1>Heat :</h1><div className="circle" style={{backgroundColor: Statusdata["heat_color"],width:20,height:20,borderRadius:'50%'}}/>
             </div>
             <div style={{display:'flex',alignItems:'center',justifyContent:'space-between'}}>
-        <h1>Heat :</h1><div className="circle" style={{backgroundColor: Statusdata["heat_color"],width:20,height:20,borderRadius:'50%'}}/>
-      </div>
-        <div style={{display:'flex',alignItems:'center',justifyContent:'space-between'}}>
-        <h1>Motion Detect :</h1><div className="circle" style={{backgroundColor: Statusdata["sensor_color"],width:20,height:20,borderRadius:'50%'}}/>
- </div>
- <div style={{display:'flex',alignItems:'center',justifyContent:'space-between', padding:80}}>
-        <h1>Safe: </h1><div className="circle" style={{backgroundColor: Safecheck["warning_color"],width:20,height:20,borderRadius:'50%'}}/>
+                <h1>Motion Detect :</h1><div className="circle" style={{backgroundColor: Statusdata["sensor_color"],width:20,height:20,borderRadius:'50%'}}/>
+             </div>
+            <div style={{display:'flex',alignItems:'center',justifyContent:'space-between', padding:80}}>
+                <h1>Safe: </h1><div className="circle" style={{backgroundColor: Safecheck["warning_color"],width:20,height:20,borderRadius:'50%'}}/>
+            </div>
         </div>
     </div>
-        </div>
 
 
     </div>
